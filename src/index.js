0b4c1de640502358c3d0de844986ea37461bf9ac
rui-watanabe <wataru610806@gmail.com>
2020-03-21 11:05:35 +0900

    implement with connect function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { Provider } from 'redux';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import reducer from './reducers';
@@ -11,10 +11,10 @@ import * as serviceWorker from './serviceWorker';
 const store = createStore(reducer);
 
 ReactDOM.render(
-<Provider store={store}>
-  <App />
-</Provider>,
-document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
